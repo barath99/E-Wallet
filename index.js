52f177b0c55b4703e26d85e1ff860bca5e2d8670
BARATH KUMAR S <50653477+barath99@users.noreply.github.com>
2019-06-11 21:31:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+	document.getElementById("chk-btn1").style.borderBottomColor = "#064acb";
+	document.getElementById("register-form").style.display = "none";
+function fun1(){
+	setTimeout(window.open("login.html","_self"),1000);
+};
+function func_loginBtn(){
+	document.getElementById("login-form").style.display = "block";
+	document.getElementById("register-form").style.display = "none";
+	document.getElementById("chk-btn1").style.borderBottomColor = "#064acb";
+	document.getElementById("chk-btn2").style.borderBottomColor = "transparent";
 
+}
+function func_registerBtn(){
+	document.getElementById("login-form").style.display = "none";
+	document.getElementById("register-form").style.display = "block";
+	document.getElementById("chk-btn1").style.borderBottomColor = "transparent";
+	document.getElementById("chk-btn2").style.borderBottomColor = "#064acb";
+}
